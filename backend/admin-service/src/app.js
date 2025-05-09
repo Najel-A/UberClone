cf3926818fde3c1a8232012847a242f038ffe4b2
kevincakey <57571360+kevincakey@users.noreply.github.com>
2025-05-09 14:57:49 -0700

    Revert "Merge branch 'admin-frontend' into main"
    This reverts commit 9f2171f405eff97affdd0bd51aba4c6f0b951598, reversing
    changes made to 427ec915ca0555b02afcb3bad3e47efbfa4830de.

--- backend/admin-service/src/app.js
+++ backend/admin-service/src/app.js
@@ -1,34 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const adminRoutes = require('./routes/adminRoutes');
+const path = require('path');
+const session = require('express-session');
 
-const adminRoutes = require("./routes/adminRoutes");
-const path = require("path");
-const session = require("express-session");
-
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true, // optional: if you're using cookies or sessions
-  })
-);
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "your-secret-key",
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your-secret-key',
     resave: false,
     saveUninitialized: false,
-    cookie: {
+    cookie: { 
       secure: false, // Set to true if using HTTPS
-      maxAge: 3600000, // 1 hour
-    },
-  })
-);
+      maxAge: 3600000 // 1 hour
+    }
+  }));
 
-app.use("/api/admin", adminRoutes);
+
+app.use('/api/admin', adminRoutes);
 
 module.exports = app;
--- frontend/admin-frontend/src/utils/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-// src/utils/axiosConfig.js
-import axios from "axios";
-import { apiUrl } from "./config";
-
-// Configure Axios globally
-axios.defaults.baseURL = apiUrl; // Your API URL
-axios.defaults.withCredentials = true; // Send credentials (cookies) with every request
-
-export default axios;
