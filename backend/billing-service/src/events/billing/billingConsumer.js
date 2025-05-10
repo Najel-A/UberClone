9644d829f8d94ff0b67743af714bc330305cd168
akasht29 <akasht9000@gmail.com>
2025-05-10 01:31:33 -0700

    bills completed kafka implementation

--- backend/billing-service/src/events/billing/billingConsumer.js
+++ backend/billing-service/src/events/billing/billingConsumer.js
@@ -1,7 +1,9 @@
 const { createConsumer, consumerGroups, topics } = require("../../config/kafka");
 const Bill = require("../../models/Bill");
-
+const CustomerWallet = require("../../models/CustomerWallet");
+const DriverWallet = require("../../models/DriverWallet");
 const consumer = createConsumer(consumerGroups.BILLING_EVENTS);
+const sequelize = require('../../config/db');
 
 exports.startBillingConsumer = async () => {
   try {
@@ -11,10 +13,33 @@ exports.startBillingConsumer = async () => {
 
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
+        const t = await sequelize.transaction();
         try {
           const { ride } = JSON.parse(message.value.toString());
-          console.log(ride);
-          
+          const customerWallet = await CustomerWallet.findOne({
+            where: { ssn: ride.customerId },
+            transaction: t,
+            lock: t.LOCK.UPDATE,
+          });
+
+          const driverWallet = await DriverWallet.findOne({
+            where: { ssn: ride.driverId },
+            transaction: t,
+            lock: t.LOCK.UPDATE,
+          });
+          if (!customerWallet) {
+            throw new Error(`Customer wallet not found for ssn: ${ride.customerId}`);
+          }
+          if (!driverWallet) {
+            throw new Error(`Driver wallet not found for ssn: ${ride.driverId}`);
+          }
+          // Transfer money
+          customerWallet.wallet -= ride.price;
+          driverWallet.wallet += ride.price;
+
+          await customerWallet.save({ transaction: t });
+          await driverWallet.save({ transaction: t });
+
           await Bill.create({
             date: ride.createdAt,
             pickupTime: ride.dateTime,
@@ -27,7 +52,8 @@ exports.startBillingConsumer = async () => {
             customerId: ride.customerId,
             status: "completed"
           });
-
+          await t.commit();
+          console.log("✅ Transaction: Wallets updated & bill inserted");
           console.log("✅ Bill inserted into MySQL via Sequelize");
         } catch (err) {
           console.error("❌ Failed to save bill:", err.message);
