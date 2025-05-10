e5bf81973179b2ba3918ec36d428d62620673d97
Devesh Singh <deveshs2015@gmail.com>
2025-05-09 18:08:03 -0700

    deleted backend

--- backend/billing-service/src/routes/billRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const billController = require('../controllers/billController');
-
-router.post('/', billController.createBill);
-router.delete('/:id', billController.deleteBill);
-router.get('/:id', billController.searchBill);
-
-module.exports = router;
--- backend/customer-service/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./src/app');
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => console.log('Customer service running on port ', process.env.PORT));
-  })
-  .catch(err => console.error('DB connection failed', err));
