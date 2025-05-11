8bd670f0a1ea3514cb3a2738e8e450bfb0d982e7
akasht29 <akasht9000@gmail.com>
2025-05-10 20:28:01 -0700

    add get routes for wallet for driver and customer

--- backend/billing-service/src/routes/billRoutes.js
+++ backend/billing-service/src/routes/billRoutes.js
@@ -5,4 +5,6 @@ const billController = require('../controllers/billController');
 router.post('/addToCustomerWallet', billController.addToCustomerWallet);
 router.post('/addToDriverWallet', billController.withdrawFromDriverWallet);
 router.post('/customerWalletCheck', billController.checkCustomerWallet);
+router.get('/getDriverWallet/:ssn', billController.getDriverWallet);
+router.get('/getCustomerWallet/:ssn', billController.getCustomerWallet);
 module.exports = router;
